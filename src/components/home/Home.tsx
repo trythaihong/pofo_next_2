a797ce6edb47345c9a0d7098eaa156cdda0fd2ce
trythaihong <hongop34@gmail.com>
2026-01-17 09:54:59 +0700

    hong

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -3,14 +3,14 @@ import React, { useEffect } from "react";
 import Hero from "./hero/Hero";
 import About from "./about/About";
 import Service from "./services/Service";
-import Project from "./project/Project";
+// import Project from "./project/Project";
 import Skill from "./skill/Skill";
 import Contact from "./contact/Contact";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import DynamicProject from "./project/dynamic";
-import Cer from "./certificate/Cer";
-import Dynamic from "./dynamic/Dynamic";
+// import DynamicProject from "./project/dynamic";
+// import Cer from "./certificate/Cer";
+// import Dynamic from "./dynamic/Dynamic";
 import Volen from "./volen/Volen";
 import Tool from "./tool/tool";
 export default function Home() {
@@ -24,12 +24,10 @@ export default function Home() {
     <div className="overflow-hidden">
       <Hero></Hero>
       <About></About>
-      <Cer></Cer>
+     
       <Volen></Volen>
       <Service></Service>
-      <Project></Project>
-      <Dynamic></Dynamic>
-      <DynamicProject></DynamicProject>
+      
       {/* <Other></Other> */}
       <Skill></Skill>
       <Tool></Tool>
